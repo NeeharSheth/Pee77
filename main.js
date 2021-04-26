0ed58e838369761d8c07fc072252f42e8946bd5f
NeeharSheth <75029741+NeeharSheth@users.noreply.github.com>
2021-04-26 19:48:56 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var Fname="Neehar";
+var Lname="Sheth :3";
+
+console.log("My 'first' console froject -_-")
+console.log("I copy-pasted everything O_O");
+console.log(Fname+Lname);
+console.log(32+23);
+console.log(32-23);
+console.log(32*23);
+console.log(42/21);
